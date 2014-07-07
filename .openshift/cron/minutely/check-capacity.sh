89357cb09f6cb5566eeb239609db3f4008cbae6c
morten@lileng.com <morten@lileng.com>
2014-07-06 20:50:10 -0500

    8 cron

--- .openshift/cron/minutely/check-capacity.sh
+++ .openshift/cron/minutely/check-capacity.sh
@@ -3,11 +3,13 @@ minute=$(date '+%M')
 hour=$(date '+%H')
 
 if [ $minute != 46 ]; then
-    exit
+   echo $minute 
+   exit
 fi
 
 if [ $hour != 21 ]; then
-    exit
+   echo $hour
+   exit
 fi
 
 date > $OPENSHIFT_JBOSSEWS_LOG_DIR/last_date_cron_ran
