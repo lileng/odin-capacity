44e71cbef539455fdb08775d48a5e89a3fb630a2
morten@lileng.com <morten@lileng.com>
2014-07-06 20:01:17 -0500

    second test cron job

--- .openshift/cron/hourly/check-capacity.sh
+++ .openshift/cron/hourly/check-capacity.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 #This is a test cron script
 #Author : Nikhil Mone
-(echo ========== Hostname ========= ; hostname ; echo ========== Date ========== ; date ; echo ========== Grep PHP procs ========= ; ps -ef | grep php ; echo =========================) > $OPENSHIFT_REPO_DIR/php/foo.txt &
+(echo ========== Hostname (hourly) ========= ; hostname ; echo ========== Date ========== ; date ; echo ========== Grep PHP procs ========= ; ps -ef | grep php ; echo =========================) > $OPENSHIFT_REPO_DIR/php/foo.txt &
 ..
 
