3e17c96a61bad94d6acbad67a24e32bc9364f8ce
morten@lileng.com <morten@lileng.com>
2014-07-06 20:15:01 -0500

    4 cron

--- .openshift/cron/minutely/check-capacity.sh
+++ .openshift/cron/minutely/check-capacity.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#This is a test cron script
-#Author : Nikhil Mone
-(echo ========== Hostname (minutely) ========= ; hostname ; echo ========== Date ========== ; date ; echo ========== Grep PHP procs ========= ; ps -ef | grep php ; echo =========================) > $OPENSHIFT_REPO_DIR/php/foo.txt &
-
+date > $OPENSHIFT_RUBY_LOG_DIR/last_date_cron_ran
