debb1c764df7791cb434c8372337bc0617130e7f
morten@lileng.com <morten@lileng.com>
2014-08-10 17:17:19 -0500

    6

--- src/main/java/odin/gateway/RestInitializer.java
+++ src/main/java/odin/gateway/RestInitializer.java
@@ -1,8 +1,17 @@
 package odin.gateway;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
  
 @ApplicationPath("/rest")
 public class RestInitializer extends Application {
+	@Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet();
+        s.add(CapacityDriver.class);
+        return s;
+    }
 
 }
