284f83c9a939b57973f82bb8d85a3d13ba6ad7dd
morten@lileng.com <morten@lileng.com>
2014-08-10 13:46:12 -0500

    Configuration object to store configuration items in the database.

--- src/main/java/odin/config/Configuration.java
+++ src/main/java/odin/config/Configuration.java
@@ -1,145 +1,127 @@
 package odin.config;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.logging.Logger;
-
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.NoResultException;
-import javax.persistence.Persistence;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.TypedQuery;
 
-import odin.domain.Sprint;
-import odin.domain.Week;
+import odin.util.DBUtil;
 
-/**
- * Sets up weeks and sprints.
- * @author mlileng
- *
- */
+@Entity
 public class Configuration {
-	private static EntityManagerFactory factory;
-	private static final String PERSISTENCE_UNIT_NAME = "individuals";
-	protected static Logger log = Logger.getLogger("Configuration");
+	@Id
+	@GeneratedValue
+	private Long id;
 
-	public static void main(String[] args) throws Exception {
-		setupSprint();
-
+	public Long getId() {
+		return id;
 	}
 
-	public static void setupWeeks() throws Exception {
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		int startWeek;
-		int finishWeek;
-		int diff;
-		SimpleDateFormat sdf;
-		Calendar calStart;
-		Calendar calEnd;
-		Date startDate;
-		Date finishDate;
-		String startDateS = "01/01/2014";
-		String finishDateS = "31/12/2014";
+	private String configInstance;
+	private String configKey;
+	private String configValue;
+	private String configDefaultValue;
+	public String getConfigDefaultValue() {
+		return configDefaultValue;
+	}
 
-		sdf = new SimpleDateFormat("dd/MM/yyyy");
+	public void setConfigDefaultValue(String configDefaultValue) {
+		this.configDefaultValue = configDefaultValue;
+	}
 
-		startDate = sdf.parse(startDateS);
-		finishDate = sdf.parse(finishDateS);
 
-		calStart = Calendar.getInstance();
-		calEnd = Calendar.getInstance();
+	public String getConfigDescription() {
+		return configDescription;
+	}
 
-		calStart.setTime(startDate);
-		startWeek = calStart.get(Calendar.WEEK_OF_YEAR);
+	public void setConfigDescription(String configDescription) {
+		this.configDescription = configDescription;
+	}
 
-		calEnd.setTime(finishDate);
-		finishWeek = calEnd.get(Calendar.WEEK_OF_YEAR);
-		if (finishWeek == 1)
-			finishWeek = 53;
+	private String configDescription;
 
-		diff = finishWeek - startWeek;
-		Calendar cal = Calendar.getInstance(Locale.US);
 
-		// Setting up DB connection
-		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-		EntityManager em = factory.createEntityManager();
+	public String getConfigInstance() {
+		return configInstance;
+	}
 
-		Week week = null;
+	public void setConfigInstance(String configInstance) {
+		this.configInstance = configInstance;
+	}
 
-		for (int i = 0; i < diff; i++) {
-			if (i == 0)
-				cal.setTime(startDate);
-			else
-				cal.add(Calendar.WEEK_OF_YEAR, 1);
+	public String getConfigKey() {
+		return configKey;
+	}
 
-			week = new Week();
+	public void setConfigKey(String configKey) {
+		this.configKey = configKey;
+	}
 
-			cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
-			log.info("WEEK " + cal.get(Calendar.WEEK_OF_YEAR) + " start: "
-					+ sdf.format(cal.getTime()));
-			week.setStartDate(cal.getTime());
+	public String getConfigValue() {
+		return configValue;
+	}
 
-			cal.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
-			log.info("WEEK " + cal.get(Calendar.WEEK_OF_YEAR) + " end  : "
-					+ sdf.format(cal.getTime()));
-			week.setEndDate(cal.getTime());
-			week.setName(cal.get(Calendar.YEAR) + "_"
-					+ cal.get(Calendar.WEEK_OF_YEAR));
+	public void setConfigValue(String configValue) {
+		this.configValue = configValue;
+	}
 
-			em.getTransaction().begin();
-			em.persist(week);
-			em.getTransaction().commit();
+	@Override
+	public String toString() {
+		return "Configuration [instance=" + getConfigInstance() + ", key=" + getConfigKey()
+				+ ", value=" + getConfigValue() + "]";
+	}
+	
+	public static String getValue(String instance ,String key) {
+		EntityManager em = DBUtil.getEntityManager();
 
-			week = null;
-		}
+		TypedQuery<Configuration> q = em.createQuery(
+				"select c from Configuration c WHERE c.configInstance=:instance AND c.configKey=:key", Configuration.class);
+		q.setParameter("instance", instance);
+		q.setParameter("key", key);
+		Configuration config = q.getSingleResult();
+		String returnValue = config.getConfigValue();
 		em.close();
+		return returnValue;
 	}
+	public static String getDefaultValue(String key) {
+		EntityManager em = DBUtil.getEntityManager();
 
-	public static void setupSprint() throws Exception {
+		TypedQuery<Configuration> q = em.createQuery(
+				"select c from Configuration c WHERE c.configInstance=:instance AND c.configKey=:key", Configuration.class);
+		q.setParameter("instance", "default");
+		q.setParameter("key", key);
+		Configuration config = q.getSingleResult();
+		String returnValue = config.getConfigValue();
+		em.close();
+		return returnValue;
+	}
+	
+	
+	public static void setValue(String instance ,String key, String value, String description) {
+		EntityManager em = DBUtil.getEntityManager();
+		Configuration conf = new Configuration();
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		// Setting up DB connection
-		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
-		// Getting Week object
-		TypedQuery<Week> q = em.createQuery(
-				"select w from Week w WHERE w.name=:name", Week.class);
-		q.setParameter("name", "2014_9");
-		Week week = q.getSingleResult();
-		log.info(week.toString());
-
-		TypedQuery<Sprint> qs = em.createQuery(
-				"select s from Sprint s WHERE s.sprintName=:sname",
-				Sprint.class);
-		qs.setParameter("sname", "Sprint 2014.02");
-		Sprint sprint = null;
-		try {
-			sprint = qs.getSingleResult();
-		} catch (NoResultException nre) {
-			log.warning("Did not find any sprint. Creating a new one...");
-			sprint = new Sprint();
-			sprint.setSprintName("Sprint 2014.02");
-			sprint.setStartDate(sdf.parse("01/02/2014"));
-			sprint.setEndDate(sdf.parse("28/02/2014"));
-
-			List<Week> weeks = new ArrayList<Week>();
-			weeks.add(week);
-			sprint.setWeeks(weeks);
-		}
-
-		week.setSprint(sprint);
-
-		em.persist(sprint);
-		em.persist(week);
+		
+		conf.setConfigInstance(instance);
+		conf.setConfigKey(key);
+		conf.setConfigValue(value);
+		conf.setConfigDefaultValue(value);
+		conf.setConfigDescription(description);
+		
+		em.persist(conf);
 		em.getTransaction().commit();
-
 		em.close();
 	}
+	
+	public static void main(String[] args){
+		Configuration.setValue("default", "gateway.sendmail.password", "$lappfi$k", "Password used on the email server.");
+	}
+
 
 }
