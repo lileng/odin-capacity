35133fbe2141f207969562dc56a3871a59c038ff
mlileng <morten@lileng.com>
2014-12-03 09:41:30 -0600

    Added ranking functionality.

--- src/main/java/odin/gateway/JiraHarvester.java
+++ src/main/java/odin/gateway/JiraHarvester.java
@@ -21,10 +21,18 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
+
+import javax.ws.rs.core.MediaType;
+
+import odin.config.Configuration;
+import odin.util.JEncrypt;
+import odin.util.OdinResponse;
 
 import org.apache.log4j.Logger;
 
@@ -32,24 +40,33 @@ import com.atlassian.jira.rest.client.api.domain.BasicIssue;
 import com.atlassian.jira.rest.client.api.domain.BasicProject;
 import com.atlassian.jira.rest.client.api.domain.BasicUser;
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.IssueField;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.api.domain.Version;
 import com.atlassian.util.concurrent.Promise;
-
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 
 /**
  * The harvester is responsible for collecting JIRA tasks from a given project.
  * 
  */
 public class JiraHarvester {
-	//private static final String String = null;
 
 	protected static Logger logger = Logger.getLogger(JiraHarvester.class);
+	static String usr = new String(JEncrypt.decode(Configuration
+			.getDefaultValue("gateway.jira.username").getBytes()));
 
-	//private static String url, usr, pw;
+	static String pw = new String(JEncrypt.decode(Configuration
+			.getDefaultValue("gateway.jira.password").getBytes()));
+
+	static String baseURL = Configuration.getDefaultValue("gateway.jira.url");
 
 	public static int collectHoursRemainingWork(String project, String sprint,
-			String assignee) throws IOException, InterruptedException, ExecutionException {
+			String assignee) throws IOException, InterruptedException,
+			ExecutionException {
 		int minutesRemainingWork = 0;
 		int hoursRemainingWork = 0;
 		Set<Hashtable<String, String>> collectedData = collect(project, sprint,
@@ -66,12 +83,13 @@ public class JiraHarvester {
 		if (minutesRemainingWork > 0) {
 			hoursRemainingWork = minutesRemainingWork / 60;
 			logger.info("hoursRemainingWork=" + hoursRemainingWork);
-		} 
+		}
 		return hoursRemainingWork;
 	}
 
 	public static Hashtable<String, String> getTasksWithRemainingWork(
-			String project, String sprint, String assignee) throws IOException, InterruptedException, ExecutionException {
+			String project, String sprint, String assignee) throws IOException,
+			InterruptedException, ExecutionException {
 		Hashtable<String, String> kv = new Hashtable<String, String>();
 		Set<Hashtable<String, String>> collectedData = collect(project, sprint,
 				assignee);
@@ -87,13 +105,124 @@ public class JiraHarvester {
 		return kv;
 	}
 
+	private static List<String> getParentKeySelection() {
+		List<String> keys = new ArrayList<String>();
+		String jql = Configuration.getDefaultValue("task.calibration.parent.selection");
+
+		int maxResults = 1000;
+		int startAt = 0;
+		logger.info("Executing JQL = " + jql);
+		logger.info("maxResults=" + maxResults + ", startAt=" + startAt);
+		Promise<SearchResult> searchResultPromise = null;
+		
+		Set<String> fields = new HashSet<String>();
+		//fields.add("key")
+		searchResultPromise = JIRAGateway.getRestClient().getSearchClient()
+				.searchJql(jql,maxResults, startAt, fields);
+
+		SearchResult searchResult = searchResultPromise.claim();
+
+		Iterable<? extends Issue> tasks = searchResult.getIssues();
+		for (Issue task : tasks) {
+			logger.info("task.key=" + task.getKey());
+			keys.add(task.getKey());
+		}
+		return keys;
+	}
+
+	public static OdinResponse updateSubtaskRanking() {
+		OdinResponse response = new OdinResponse();
+		String customRankingField1 = Configuration.getDefaultValue("task.calibration.jira.rankfield.1");
+		String customRankingField2 = Configuration.getDefaultValue("task.calibration.jira.rankfield.2");
+		List<String> parentKeys = getParentKeySelection();
+
+		for (String parentKey : parentKeys) {
+
+			// 1. Get rank from parent
+			logger.info("Get rank from parentKey=" + parentKey);
+			Issue issue = JIRAGateway.getRestClient().getIssueClient()
+					.getIssue(parentKey).claim();
+			int parentRank = 0;
+
+			if (issue.getField(customRankingField1) != null) {
+				IssueField field = issue.getField(customRankingField1);
+				parentRank = (int) (double) field.getValue(); // TODO: Catch
+				// ClassCastException or
+				// NullPointerException
+
+			} else if (issue.getField(customRankingField2) != null) {
+				IssueField field = issue.getField(customRankingField2);
+				parentRank = (int) (double) field.getValue(); // TODO: Catch
+				// ClassCastException or
+				// NullPointerException
+			}
+			logger.info("Parent key=" + parentKey + ", parentRank="
+					+ parentRank);
+
+			// 2. Get active subtasks
+			String jql = "status not in (Done, Invalid) AND parent = "
+					+ parentKey;
+			int maxResults = 1000;
+			int startAt = 0;
+			logger.info("Executing JQL = " + jql);
+			logger.info("maxResults=" + maxResults + ", startAt=" + startAt);
+			Promise<SearchResult> searchResultPromise = null;
+
+			searchResultPromise = JIRAGateway.getRestClient().getSearchClient()
+					.searchJql(jql);
+
+			SearchResult searchResult = searchResultPromise.claim();
+
+			Iterable<? extends Issue> subtasks = searchResult.getIssues();
+			for (Issue subtask : subtasks) {
+				logger.info("subtask.key=" + subtask.getKey());
+				// logger.info("getSummary = " + subtask.getSummary());
+
+				// 3. Update active subtasks with new rank
+				updateRank(subtask.getKey(), parentRank, customRankingField1); // TODO: Also make work for other custom ranking field
+			}
+			response.setReasonPhrase("OK");
+
+		}
+		return response;
+	}
+
+	private static void updateRank(String key, int rank, String rankingField) {
+
+		String url = baseURL + "/rest/api/2/issue/" + key;
+		Client client = Client.create();
+		WebResource webResource = client.resource(url);
+
+		// client.addFilter(new
+		// com.sun.jersey.api.client.filter.LoggingFilter());
+		client.addFilter(new HTTPBasicAuthFilter(usr, pw));
+		String jsonRequest = "{\n   \"fields\": { \n    \"" + rankingField + "\": "
+				+ rank + " \n   } \n }";
+		logger.info("Preparing to update url=" + url + ", with JSON="
+				+ jsonRequest);
+
+		ClientResponse clientResponse = webResource
+				.accept(MediaType.APPLICATION_JSON)
+				.header("Content-Type", "application/json")
+				.put(ClientResponse.class, jsonRequest);
+
+		// String output = clientResponse.getEntity(String.class);
+		// logger.info("Output from Server .... \n");
+		// logger.info(output);
+		if (clientResponse.getStatus() != 204) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ clientResponse.getStatus());
+		}
+	}
+
 	public static Set<Hashtable<String, String>> collect(String project,
-			String sprint, String assignee) throws IOException, InterruptedException, ExecutionException {
+			String sprint, String assignee) throws IOException,
+			InterruptedException, ExecutionException {
 
 		logger.info("Get projects in Jira");
 		// First get all projects that the user knows about in JIRA
-		Iterable<BasicProject> projects = JIRAGateway.getRestClient().getProjectClient()
-				.getAllProjects().claim();
+		Iterable<BasicProject> projects = JIRAGateway.getRestClient()
+				.getProjectClient().getAllProjects().claim();
 		for (BasicProject bp : projects) {
 			logger.info("Project name=" + bp.getName());
 		}
@@ -108,19 +237,19 @@ public class JiraHarvester {
 		int startAt = 1;
 		logger.info("Executing JQL = " + jql);
 		logger.info("maxResults=" + maxResults + ", startAt=" + startAt);
-		searchResultPromise = JIRAGateway.getRestClient().getSearchClient().searchJql(jql,
-				maxResults, startAt, null);
+		searchResultPromise = JIRAGateway.getRestClient().getSearchClient()
+				.searchJql(jql, maxResults, startAt, null);
 
 		SearchResult searchResult = searchResultPromise.get();
-		
-		if(searchResult != null) {
+
+		if (searchResult != null) {
 			Iterable<? extends Issue> issues = searchResult.getIssues();
 			Set<Hashtable<String, String>> issuesForUser = getIssuesForUser(issues);
 			JIRAGateway.closeRestClient();
 			return issuesForUser;
-		} else return null;
-		
-		
+		} else
+			return null;
+
 	}
 
 	private static Set getIssuesForUser(Iterable<? extends Issue> issues) {
@@ -133,7 +262,8 @@ public class JiraHarvester {
 			Hashtable<String, String> kv = new Hashtable<String, String>();
 			kv.put("key", bi.getKey());
 			String key = bi.getKey();
-			Issue issue = JIRAGateway.getRestClient().getIssueClient().getIssue(key).claim();
+			Issue issue = JIRAGateway.getRestClient().getIssueClient()
+					.getIssue(key).claim();
 			BasicUser bu = issue.getAssignee();
 			kv.put("summary", issue.getSummary());
 			if (bu != null) {
