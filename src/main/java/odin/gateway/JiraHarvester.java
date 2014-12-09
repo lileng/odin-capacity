d1d5da06792991fb7abf5a45ea08b9216eccd3d4
mlileng <morten@lileng.com>
2014-12-09 11:05:21 -0600

    Refactoring away backlog management functionality.

--- src/main/java/odin/gateway/JiraHarvester.java
+++ src/main/java/odin/gateway/JiraHarvester.java
@@ -21,18 +21,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-
-import javax.ws.rs.core.MediaType;
-
-import odin.config.Configuration;
-import odin.util.JEncrypt;
-import odin.util.OdinResponse;
 
 import org.apache.log4j.Logger;
 
@@ -40,14 +32,9 @@ import com.atlassian.jira.rest.client.api.domain.BasicIssue;
 import com.atlassian.jira.rest.client.api.domain.BasicProject;
 import com.atlassian.jira.rest.client.api.domain.BasicUser;
 import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.api.domain.IssueField;
 import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.api.domain.Version;
 import com.atlassian.util.concurrent.Promise;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 
 /**
  * The harvester is responsible for collecting JIRA tasks from a given project.
@@ -56,13 +43,6 @@ import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 public class JiraHarvester {
 
 	protected static Logger logger = Logger.getLogger(JiraHarvester.class);
-	static String usr = new String(JEncrypt.decode(Configuration
-			.getDefaultValue("gateway.jira.username").getBytes()));
-
-	static String pw = new String(JEncrypt.decode(Configuration
-			.getDefaultValue("gateway.jira.password").getBytes()));
-
-	static String baseURL = Configuration.getDefaultValue("gateway.jira.url");
 
 	public static int collectHoursRemainingWork(String project, String sprint,
 			String assignee) throws IOException, InterruptedException,
@@ -105,119 +85,6 @@ public class JiraHarvester {
 		return kv;
 	}
 
-	private static List<String> getParentKeySelection() {
-		List<String> keys = new ArrayList<String>();
-		String jql = Configuration.getDefaultValue("task.calibration.parent.selection");
-
-		int maxResults = 1000;
-		int startAt = 0;
-		logger.info("Executing JQL = " + jql);
-		logger.info("maxResults=" + maxResults + ", startAt=" + startAt);
-		Promise<SearchResult> searchResultPromise = null;
-		
-		Set<String> fields = new HashSet<String>();
-		//fields.add("key")
-		searchResultPromise = JIRAGateway.getRestClient().getSearchClient()
-				.searchJql(jql,maxResults, startAt, fields);
-
-		SearchResult searchResult = searchResultPromise.claim();
-
-		Iterable<? extends Issue> tasks = searchResult.getIssues();
-		for (Issue task : tasks) {
-			logger.info("task.key=" + task.getKey());
-			keys.add(task.getKey());
-		}
-		return keys;
-	}
-
-	public static OdinResponse updateSubtaskRanking() {
-		OdinResponse response = new OdinResponse();
-		String customRankingField1 = Configuration.getDefaultValue("task.calibration.jira.rankfield.1");
-		String customRankingField2 = Configuration.getDefaultValue("task.calibration.jira.rankfield.2");
-		List<String> parentKeys = getParentKeySelection();
-
-		for (String parentKey : parentKeys) {
-
-			// 1. Get rank from parent
-			logger.info("Get rank from parentKey=" + parentKey);
-			Issue issue = JIRAGateway.getRestClient().getIssueClient()
-					.getIssue(parentKey).claim();
-			int parentRank = 0;
-
-			if (issue.getField(customRankingField1) != null) {
-				IssueField field = issue.getField(customRankingField1);
-				parentRank = (int) (double) field.getValue(); // TODO: Catch
-				// ClassCastException or
-				// NullPointerException
-
-			} else if (issue.getField(customRankingField2) != null) {
-				IssueField field = issue.getField(customRankingField2);
-				parentRank = (int) (double) field.getValue(); // TODO: Catch
-				// ClassCastException or
-				// NullPointerException
-			}
-			logger.info("Parent key=" + parentKey + ", parentRank="
-					+ parentRank);
-
-			// 2. Get active subtasks
-			String jql = "status not in (Done, Invalid) AND parent = "
-					+ parentKey;
-			int maxResults = 1000;
-			int startAt = 0;
-			logger.info("Executing JQL = " + jql);
-			logger.info("maxResults=" + maxResults + ", startAt=" + startAt);
-			Promise<SearchResult> searchResultPromise = null;
-
-			searchResultPromise = JIRAGateway.getRestClient().getSearchClient()
-					.searchJql(jql);
-
-			SearchResult searchResult = searchResultPromise.claim();
-
-			Iterable<? extends Issue> subtasks = searchResult.getIssues();
-			for (Issue subtask : subtasks) {
-				logger.info("subtask.key=" + subtask.getKey());
-				// logger.info("getSummary = " + subtask.getSummary());
-
-				// 3. Update active subtasks with new rank
-				if (issue.getField(customRankingField1) != null) {
-					updateRank(subtask.getKey(), parentRank, customRankingField1);
-				} else if(issue.getField(customRankingField2) != null) {
-					updateRank(subtask.getKey(), parentRank, customRankingField2);
-				}
-			}
-			response.setReasonPhrase("OK");
-
-		}
-		return response;
-	}
-
-	private static void updateRank(String key, int rank, String rankingField) {
-
-		String url = baseURL + "/rest/api/2/issue/" + key;
-		Client client = Client.create();
-		WebResource webResource = client.resource(url);
-
-		// client.addFilter(new
-		// com.sun.jersey.api.client.filter.LoggingFilter());
-		client.addFilter(new HTTPBasicAuthFilter(usr, pw));
-		String jsonRequest = "{\n   \"fields\": { \n    \"" + rankingField + "\": "
-				+ rank + " \n   } \n }";
-		logger.info("Preparing to update url=" + url + ", with JSON="
-				+ jsonRequest);
-
-		ClientResponse clientResponse = webResource
-				.accept(MediaType.APPLICATION_JSON)
-				.header("Content-Type", "application/json")
-				.put(ClientResponse.class, jsonRequest);
-
-		// String output = clientResponse.getEntity(String.class);
-		// logger.info("Output from Server .... \n");
-		// logger.info(output);
-		if (clientResponse.getStatus() != 204) {
-			throw new RuntimeException("Failed : HTTP error code : "
-					+ clientResponse.getStatus());
-		}
-	}
 
 	public static Set<Hashtable<String, String>> collect(String project,
 			String sprint, String assignee) throws IOException,
