e7f4e86adcfa6c33c421079eff9f1765cf328ed0
MortenLileng <mol@lileng.com>
2015-08-15 14:24:39 -0500

    Main driver for odin-capacity

---
+++ src/main/java/odin/capacity/Main.java
@@ -0,0 +1,51 @@
+package odin.capacity;
+
+import org.apache.log4j.Logger;
+
+import odin.demand.ManageBacklog;
+import odin.gateway.ReportAllocation;
+import odin.util.ReadGoogleSpreadsheet;
+
+/**
+ * Main starting point for Odin Capacity. Command line args kicking off 
+ * the different tasks.
+ *
+ */
+public class Main {
+	protected static Logger LOG = Logger
+			.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		if(args[0].equals("ReadGoogleSpreadsheet")){
+			LOG.info("Starting ReadGoogleSpreadsheet");
+			try {
+				ReadGoogleSpreadsheet.main(args);
+			} catch (Exception e) {
+				LOG.error("Error in ReadGoogleSpreadsheet: " + e);
+				e.printStackTrace();
+			}
+			LOG.info("Stopping ReadGoogleSpreadsheet");
+		} else if(args[0].equals("ManageBacklog")){
+			LOG.info("Starting ManageBacklog");
+			try {
+				ManageBacklog.main(args);
+			} catch (Exception e) {
+				LOG.error("Error in ManageBacklog: " + e);
+				e.printStackTrace();
+			}
+			LOG.info("Stopping ManageBacklog");
+		} else if(args[0].equals("ReportAllocation")){
+			LOG.info("Starting ReportAllocation");
+			try {
+				ReportAllocation.main(args);
+			} catch (Exception e) {
+				LOG.error("Error in ReportAllocation: " + e);
+				e.printStackTrace();
+			}
+			LOG.info("Stopping ReportAllocation");
+		}
+		else
+			LOG.warn("No work done. Could not understand input variables.");
+	}
+
+}
